7939bd769352feb88e1a5b9c301c806c9547d182
shu0121 <kym0121shu@gmail.com>
2023-01-11 22:44:12 +0900

    途中経過

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+//色
+const Color primaryColor = Color(0xffaaaaaa);
+
+const TextStyle titleStyle = TextStyle(color: Colors.black, fontSize: 22, fontWeight: FontWeight.bold,);
--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,158 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_challenge01/constants.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Travel',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        appBarTheme: const AppBarTheme(
+          // color: Colors.grey,
+          elevation: 0,
+          iconTheme: IconThemeData(
+            color: Colors.grey,
+          ),
+        ),
+        primaryColor: primaryColor,
+        scaffoldBackgroundColor: Colors.white,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SingleChildScrollView(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          children: [
+            Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20),
+                  ),
+                  child: Image.asset('images/pracePic/IMG_2237.JPG'),
+                ),
+                SafeArea(
+                    child:Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          IconButton(onPressed: (){}, icon: Icon(Icons.share), color: Colors.white,),
+                          IconButton(onPressed: (){}, icon: Icon(Icons.favorite_border), color: Colors.white,)
+                        ],
+                      ),
+                    ),
+                ),
+                Container(
+                  height: 350,
+                  padding: EdgeInsets.only(
+                    left: 15.0,
+                    bottom: 95.0,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Bali', style: titleStyle,),
+                      Row(
+                          children: [
+                            Icon(Icons.place, color: primaryColor,),
+                            Text('Indonesia', style: titleStyle,),
+                          ],
+                        ),
+                      Row(
+                        children: [
+                          Icon(Icons.star, color: primaryColor,),
+                          Icon(Icons.star, color: primaryColor,),
+                          Icon(Icons.star, color: primaryColor,),
+                          Icon(Icons.star, color: primaryColor,),
+                          Icon(Icons.star_border, color: primaryColor,),
+                          Text(' '),
+                          Text('4.0', style: titleStyle,),
+                        ],
+                      ),
+                    ],
+                  ),
+
+                ),
+              ],
             ),
           ],
+          // children: ClipRRect(
+          //   borderRadius: BorderRadius.circular(30),
+          //   child: Image.asset('images/pracePic/IMG_2237.JPG'),
+          // ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         actions: [
+//           IconButton(onPressed: (){}, icon: Icon(Icons.navigate_before_outlined), color: Colors.white,),
+//           IconButton(onPressed: (){}, icon: Icon(Icons.favorite_border), color: Colors.white,)
+//
+//         ],
+//       ),
+//       body: Container(
+//         decoration: BoxDecoration(
+//           image: DecorationImage(
+//             image: AssetImage('images/pracePic/IMG_2237.JPG'),
+//           ),
+//           border: Border.all(color: Colors.white, width: 3,),
+//           borderRadius: BorderRadius.circular(50),
+//         ),
+//         child: Column(
+//           children: [
+//                 Text('Bali', style: titleStyle,),
+//                 Container(
+//                   child: Row(
+//                     children: [
+//                       Icon(Icons.place, color: primaryColor,),
+//                       Text('Indonesia', style: titleStyle,),
+//                     ],
+//                   ),
+//                 ),
+//             Container(
+//               child: Row(
+//                 children: [
+//                   Icon(Icons.star, color: primaryColor,),
+//                   Icon(Icons.star, color: primaryColor,),
+//                   Icon(Icons.star, color: primaryColor,),
+//                   Icon(Icons.star, color: primaryColor,),
+//                   Icon(Icons.star_border, color: primaryColor,),
+//                   Text('4.0', style: titleStyle,),
+//                 ],
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
